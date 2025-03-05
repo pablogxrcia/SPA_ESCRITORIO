e92fa240802c17101c022d53b43a62d76b136525
pablogxrcia <paabloogarcia05@gmail.com>
2025-03-05 10:36:44 +0100

    iconos InterfazCarreras

---
+++ src/main/java/controlador/ControladorCarrera.java
@@ -0,0 +1,67 @@
+package controlador;
+
+import api.RepositoryUsers;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import modelo.LoginRequest;
+import modelo.UserLogin;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControladorCarrera implements Initializable {
+    @FXML
+    private ImageView imgAddCarrera;
+
+    @FXML
+    private ImageView imgCerrar;
+
+    @FXML
+    private ImageView imgLogo;
+
+    @FXML
+    private ImageView imgUsers;
+
+    @FXML
+    private ListView<?> lstCarreras;
+
+    @FXML
+    private RadioButton optCiclismo;
+
+    @FXML
+    private RadioButton optRunning;
+
+    @FXML
+    private RadioButton optTodas;
+
+    @FXML
+    private RadioButton optTrailRunning;
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        establecerIconos();
+
+    }
+    private void establecerIconos(){
+        imgLogo.setImage(new Image(getClass().getClassLoader().getResourceAsStream("images/logoApp.png")));
+        imgAddCarrera.setImage(new Image(getClass().getClassLoader().getResourceAsStream("images/añadir.png")));
+        imgCerrar.setImage(new Image(getClass().getClassLoader().getResourceAsStream("images/cerrar-sesion.png")));
+        imgUsers.setImage(new Image(getClass().getClassLoader().getResourceAsStream("images/login.png")));
+    }
+
+
+}
