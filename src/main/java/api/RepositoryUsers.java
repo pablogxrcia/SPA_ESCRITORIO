39ab816ab2da11998edba1fe2c2627ddd7d83f5f
pablogxrcia <paabloogarcia05@gmail.com>
2025-03-06 08:46:15 +0100

    Mostrar carreras ListView

--- src/main/java/api/RepositoryUsers.java
+++ src/main/java/api/RepositoryUsers.java
@@ -10,7 +10,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RepositoryUsers {
-    String baseUrl = "http://192.168.50.143:3000/api/users/";
+    String baseUrl = "http://44.203.132.49:3000/api/users/";
     Gson gson = new GsonBuilder()
             .setLenient()
             .create();
